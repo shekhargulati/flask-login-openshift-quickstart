6b354956383d103a14bc15a83fb59fb09db8aed5
Shekhar Gulati <shekhargulati84@gmail.com>
2013-08-22 17:12:30 +0530

    Flask Todo app

---
+++ wsgi/todoapp.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from flask import Flask, request, flash, url_for, redirect, \
+     render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_pyfile('todoapp.cfg')
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column('todo_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
+    done = db.Column(db.Boolean)
+    pub_date = db.Column(db.DateTime)
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.done = False
+        self.pub_date = datetime.utcnow()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+        todos=Todo.query.order_by(Todo.pub_date.desc()).all()
+    )
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['title']:
+            flash('Title is required', 'error')
+        elif not request.form['text']:
+            flash('Text is required', 'error')
+        else:
+            todo = Todo(request.form['title'], request.form['text'])
+            db.session.add(todo)
+            db.session.commit()
+            flash(u'Todo item was successfully created')
+            return redirect(url_for('index'))
+    return render_template('new.html')
+
+@app.route('/todos/<int:todo_id>', methods = ['GET' , 'POST'])
+def show_or_update(todo_id):
+    todo_item = Todo.query.get(todo_id)
+    if request.method == 'GET':
+        return render_template('view.html',todo=todo_item)
+    todo_item.title = request.form['title']
+    todo_item.text  = request.form['text']
+    todo_item.done  = ('done.%d' % todo_id) in request.form
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run()
